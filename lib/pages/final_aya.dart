975956a2160a6534dcb5c3976783d2d1c7f49f6f
Ammar <ammarlouah9@gmail.com>
2022-04-18 14:44:56 +0000

    Add scroll feature

--- lib/pages/final_aya.dart
+++ lib/pages/final_aya.dart
@@ -20,81 +20,83 @@ class _FinalAyaState extends State<FinalAya> {
       backgroundColor: Colors.grey[900],
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'الآية',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30,
-                fontFamily: 'Lateef',
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 60),
-            Card(
-              margin: EdgeInsets.all(10.0),
-              child: ListTile(
-                title: Text(
-                  aya,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 50,
-                    fontFamily: 'Lateef',
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            FlatButton(
-              color: Colors.white,
-              child: Text(
-                'نسخ الآية',
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'الآية',
                 style: TextStyle(
-                  fontSize: 20,
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontFamily: 'Lateef',
                 ),
+                textAlign: TextAlign.center,
               ),
-              onPressed: () {
-                setState(() {
-                  FlutterClipboard.copy(aya);
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: const Text(
-                        'تم نسخ الآية إ',
-                        style: TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                      /*action: SnackBarAction(
-                      label: 'Action',
-                      onPressed: () {},
-                    ),*/
+              SizedBox(height: 60),
+              Card(
+                margin: EdgeInsets.all(10.0),
+                child: ListTile(
+                  title: Text(
+                    aya,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 50,
+                      fontFamily: 'Lateef',
                     ),
-                  );
-                });
-              },
-            ),
-            SizedBox(height: 20),
-            FlatButton(
-              color: Colors.white,
-              child: Text(
-                'ابحث عن آية أخرى',
-                style: TextStyle(
-                  fontSize: 20,
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ),
-              onPressed: () {
-                setState(() {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => AyahGenerator()),
-                  );
-                });
-              },
-            ),
-          ],
+              SizedBox(height: 20),
+              FlatButton(
+                color: Colors.white,
+                child: Text(
+                  'نسخ الآية',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                onPressed: () {
+                  setState(() {
+                    FlutterClipboard.copy(aya);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: const Text(
+                          'تم نسخ الآية إ',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        /*action: SnackBarAction(
+                        label: 'Action',
+                        onPressed: () {},
+                      ),*/
+                      ),
+                    );
+                  });
+                },
+              ),
+              SizedBox(height: 20),
+              FlatButton(
+                color: Colors.white,
+                child: Text(
+                  'ابحث عن آية أخرى',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                onPressed: () {
+                  setState(() {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => AyahGenerator()),
+                    );
+                  });
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
